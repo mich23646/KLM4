0f6343a107cf4a144753f56cc4bcac1ea25c7723
Michiel Rhee <michiel_rhee@hotmail.com>
2016-04-26 15:01:22 +0200

    Checklist multiple buttons on each topic

--- KLM4/src/klm4/ScreenControllers/FXMLChecklistScreenController.java
+++ KLM4/src/klm4/ScreenControllers/FXMLChecklistScreenController.java
@@ -7,7 +7,10 @@ package klm4.ScreenControllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
 
 /**
  * FXML Controller class
@@ -15,13 +18,52 @@ import javafx.fxml.Initializable;
  * @author Michiel
  */
 public class FXMLChecklistScreenController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private ToggleButton toggleButtonNegative;
+    @FXML
+    private ToggleButton toggleButtonNeutral;
+    @FXML
+    private ToggleButton toggleButtonPositive;
+    
+   
+    @FXML
+    private void HandleButtonNegative(ActionEvent event) {
+        if(toggleButtonNegative.isSelected()){
+            toggleButtonNegative.setId("ButtonNegativePressed");            
+      }
+        else{
+            toggleButtonNegative.setId("ButtonNegative");
+        }
+        toggleButtonNeutral.setSelected(false); toggleButtonNeutral.setId("ButtonNeutral");
+        toggleButtonPositive.setSelected(false); toggleButtonPositive.setId("ButtonPositive");
+    }
+    @FXML
+    private void HandleButtonNeutral(ActionEvent event) {
+        if(toggleButtonNeutral.isSelected()){
+            toggleButtonNeutral.setId("ButtonNeutralPressed");
+      }
+        else{
+            toggleButtonNeutral.setId("ButtonNeutral");
+        }
+        toggleButtonNegative.setSelected(false); toggleButtonNegative.setId("ButtonNegative");
+        toggleButtonPositive.setSelected(false); toggleButtonPositive.setId("ButtonPositive");
+    }
+    @FXML
+    private void HandleButtonPositive(ActionEvent event) {
+        if(toggleButtonPositive.isSelected()){
+            toggleButtonPositive.setId("ButtonPositivePressed");
+      }
+        else{
+            toggleButtonPositive.setId("ButtonPositive");
+        }
+        toggleButtonNeutral.setSelected(false); toggleButtonNeutral.setId("ButtonNeutral");
+        toggleButtonNegative.setSelected(false); toggleButtonNegative.setId("ButtonNegative");
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    
     
 }
