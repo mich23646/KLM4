990e06367540539934bedf950cb9d24d772db388
kabir <zub2zero@gmail.com>
2016-05-17 14:36:51 +0200

    update
    update

--- KLM4/src/klm4/ScreenControllers/FXMLQRScanner.java
+++ KLM4/src/klm4/ScreenControllers/FXMLQRScanner.java
@@ -5,8 +5,22 @@
  */
 package klm4.ScreenControllers;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +28,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -32,9 +47,27 @@ public class FXMLQRScanner implements Initializable {
         File showOpenDialog = fileChooser.showOpenDialog(stage); 
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    public static void main(String[] args) throws WriterException, IOException,
+   NotFoundException {
+  String filePath = "D:/qrcode.35189288.png";
+  String charset = "UTF-8"; // or "ISO-8859-1"
+  Map hintMap = new HashMap();
+  hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+  System.out.println("Data read from QR Code: "
+    + readQRCode(filePath, charset, hintMap));
+ }
+
+ public static String readQRCode(String filePath, String charset, Map hintMap)
+   throws FileNotFoundException, IOException, NotFoundException {
+  BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(
+    new BufferedImageLuminanceSource(
+      ImageIO.read(new FileInputStream(filePath)))));
+  Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap,
+    hintMap);
+  return qrCodeResult.getText();
+ }
 }
+      
+    
+
